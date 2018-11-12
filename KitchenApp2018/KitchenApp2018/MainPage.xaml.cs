8ba1844ea69317cf73784b98ef92d72274ff8249
Nomijee <33327266+Nomijee@users.noreply.github.com>
2018-11-12 21:36:01 +0000

    add buttons and create content page
    add menu,pizzas burger and other tabs on main page and navigate them to there pages

---
+++ KitchenApp2018/KitchenApp2018/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace KitchenApp2018
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void menubtn_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new menu());
+        }
+
+        private void specialBtn_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new specials());
+        }
+        private void pizzasBtn_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Pizzas());
+        }
+        private void wrapsBtn_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new wraps());
+        }
+        private void burgersBtn_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new burgers());
+        }
+        private void desertsBtn_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new deserts());
+        }
+
+
+    }
+}
